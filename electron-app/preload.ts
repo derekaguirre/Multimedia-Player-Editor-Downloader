46b4b320d0129ed6e35379c51a561ac4447f4e6d
derekaguirre <derekaguirre@me.com>
2023-06-18 00:38:38 -0600

    Added Electron and Typescript files

---
+++ electron-app/preload.ts
@@ -0,0 +1,8 @@
+// All of the Node.js APIs are available in the preload process.
+// It has the same sandbox as a Chrome extension.
+window.addEventListener("DOMContentLoaded", () =>{
+    //We have some window and node.js access here to do stuff on inintial load
+});
+
+export { };
+
