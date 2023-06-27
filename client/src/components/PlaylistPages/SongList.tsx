a0d59af29b064e592a68d8ce769bb9a629189743
derekaguirre <derekaguirre@me.com>
2023-06-27 06:46:27 -0600

    Updated table sorting logic

--- client/src/components/PlaylistPages/SongList.tsx
+++ client/src/components/PlaylistPages/SongList.tsx
@@ -1,6 +1,6 @@
 import axios, { AxiosResponse } from "axios";
 import React, { useEffect, useState } from "react";
-import ReactPlayer from "react-player/";
+import ReactPlayer from "react-player";
 
 const API_URL = "http://localhost:4000";
 
@@ -11,13 +11,19 @@ interface FileObject {
   filePath: string;
   fileSize: number;
   fileType: string;
+  sortOrder?: "asc" | "desc";
+}
+interface SortArrowProps {
+  order?: "asc" | "desc";
 }
 
-
-const PlaylistPage = () => {
-  //Set up the states for retrieving and playing songs
+const PlaylistPage: React.FC = () => {
   const [songs, setSongs] = useState<FileObject[]>([]);
   const [playingFile, setPlayingFile] = useState<string | null>(null);
+  const [sortColumn, setSortColumn] = useState<keyof FileObject | "">("");
+  const [sortDirection, setSortDirection] = useState<"asc" | "desc">("asc");
+
+  
 
   useEffect(() => {
     fetchPlaylistData();
@@ -25,51 +31,105 @@ const PlaylistPage = () => {
 
   const fetchPlaylistData = async () => {
     try {
-      const response: AxiosResponse<FileObject[]> = await axios.get(`${API_URL}/files`);
+      const response: AxiosResponse<FileObject[]> = await axios.get(
+        `${API_URL}/files`
+      );
       setSongs(response.data);
     } catch (error) {
       console.error("Error fetching playlist data:", error);
     }
   };
 
-  //Pass the formatted file path through here
   const handlePlay = (file: string) => {
     console.log("Can file play? ", ReactPlayer.canPlay(file));
     console.log("Attempting to play: ", file);
     setPlayingFile(file);
   };
 
-  //antd
-  //react-player
+  const SortArrow: React.FC<SortArrowProps> = ({ order }) => (
+    <span>{order === "asc" ? "▲" : order === "desc" ? "▼" : ""}</span>
+  );
+
+  const handleSort = (column: keyof FileObject) => {
+    if (!column) return;
+
+    setSongs((prevSongs) => {
+      const isAsc = sortColumn === column && sortDirection === "asc";
+      const sortedSongs = [...prevSongs].sort((a, b) => {
+        const aValue =
+          typeof a[column] === "number" ? a[column]?.toString() : a[column];
+        const bValue =
+          typeof b[column] === "number" ? b[column]?.toString() : b[column];
+
+        if (aValue && bValue) {
+          if (aValue < bValue) return isAsc ? -1 : 1;
+          if (aValue > bValue) return isAsc ? 1 : -1;
+        }
+        return 0;
+      });
+
+      sortedSongs.forEach((song) => {
+        song.sortOrder = song === sortedSongs[0] ? sortDirection : undefined;
+      });
+
+      return sortedSongs;
+    });
+
+    setSortColumn(column);
+    setSortDirection(
+      sortColumn === column ? (sortDirection === "asc" ? "desc" : "asc") : "asc"
+    );
+  };
+
   return (
     <div>
       <h1>Playlist</h1>
-      <div className ="filePlayer">
-        {playingFile && (<ReactPlayer url={playingFile} playing={true} controls={true} width="100%"height="100px"/>)}
+      <div className="filePlayer">
+        {playingFile && (
+          <ReactPlayer
+            url={playingFile}
+            playing
+            controls
+            width="100%"
+            height="100px"
+          />
+        )}
       </div>
 
-      <div className= "playlistTable" data-scroll="0">
+      <div className="playlistTable">
         <table>
           <thead>
             <tr>
-              <th>FileID</th>
-              <th>File Name</th>
-              <th>File Path</th>
-              <th>File Size</th>
-              <th>File Type</th>
+              <th onClick={() => handleSort("_id")}>
+                FileID <SortArrow order={sortColumn === "_id" ? sortDirection : undefined} />
+              </th>
+              <th onClick={() => handleSort("fileNameOriginal")}>
+                File Name <SortArrow order={sortColumn === "fileNameOriginal" ? sortDirection : undefined} />
+              </th>
+              <th onClick={() => handleSort("filePath")}>
+                File Path <SortArrow order={sortColumn === "filePath" ? sortDirection : undefined} />
+              </th>
+              <th onClick={() => handleSort("fileSize")}>
+                File Size <SortArrow order={sortColumn === "fileSize" ? sortDirection : undefined} />
+              </th>
+              <th onClick={() => handleSort("fileType")}>
+                File Type <SortArrow order={sortColumn === "fileType" ? sortDirection : undefined} />
+              </th>
               <th></th>
             </tr>
           </thead>
           <tbody>
             {songs.map((file) => (
               <tr key={file._id}>
-                <td>{file._id} </td>
+                <td>{file._id}</td>
                 <td>{file.fileNameOriginal}</td>
                 <td>{file.filePath}</td>
                 <td>{file.fileSize}</td>
                 <td>{file.fileType}</td>
                 <td id="playButtonEntry">
-                  <button onClick={() => handlePlay(`${API_URL}/uploads/${file.fileNameFormatted}`) }>
+                  <button
+                    onClick={() => handlePlay(`${API_URL}/uploads/${file.fileNameFormatted}`)}
+                  >
                     Play
                   </button>
                 </td>
