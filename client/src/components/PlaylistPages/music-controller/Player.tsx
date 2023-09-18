f9be65256f56c3cf75e3f3716cba6dc04b7b3614
derekaguirre <derekaguirre@me.com>
2023-09-17 22:08:10 -0600

    Removed remaining react-player code

--- client/src/components/PlaylistPages/music-controller/Player.tsx
+++ client/src/components/PlaylistPages/music-controller/Player.tsx
@@ -1,6 +1,5 @@
 import axios from "axios";
 import React, { MouseEvent, useContext, useEffect, useState } from "react";
-// import ReactPlayer from "react-player";
 import { PlayerContext } from "../../PlayerContext";
 // import { Howl, Howler } from 'howler';
 import { SongObject, SongsContext } from "../../SongsContext";
@@ -31,21 +30,7 @@ const Player: React.FC = () => {
 
   return (
     <div>Player</div>
-    // <ReactPlayer
-    //   className="react-player"
-    //   url={activeSong || ""}
-    //   playing={true}
-    //   controls={true}
-    //   width="640px"
-    //   height="360px"
-    //   volume={0.4}
-    //   onEnded={() => {
-    //     console.log("onEnded");
-    //   }}
-    //   onError={(error) => {
-    //     console.error("Error playing media:", error);
-    //   }}
-    // />
+    
   );
 };
 export default Player;
