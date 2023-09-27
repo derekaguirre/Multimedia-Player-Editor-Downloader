7c391cfe11ea3a5e48ca495a6fbc09ae9e0d4532
derekaguirre <derekaguirre@me.com>
2023-09-27 05:14:14 -0600

    Completed main seeking functionality

--- client/src/components/PlaylistPages/music-controller/time-controls/PlayBar.tsx
+++ client/src/components/PlaylistPages/music-controller/time-controls/PlayBar.tsx
@@ -1,41 +1,37 @@
 import { Howl } from "howler";
 import React, { useEffect, useRef, useState } from "react";
 
-//TODO seek bar moves every interval by itself. so disable seek calculation when holding the bar
 //TODO add a div wrapper over seekbar that extends the range of initiating a seek by 4px but is invis
-//TODO fix the progress bar sliding off the end of the seek bar
-//TODO add seek on click too
-//TODO seek sometimes works every other drag, it would just snap to original location and ignore the drag
+//TODO replace isDragging with isSeeking from parent component
+//TODO disable highlighting of other components while dragging the bar
+//TODO Allow for progress bar to be dragged even if the cursor leaves the component
 
-// Howl needed for seek
 interface TimeProps {
   currentHowl: Howl | null;
   currentTime: number;
   fullDuration: number | null;
+  setCurrentTime: (time: number) => void;
+  setIsSeeking: (isSeeking: boolean) => void;
 }
 
 const PlaybackBar: React.FC<TimeProps> = ({
   currentHowl,
   currentTime,
   fullDuration,
+  setCurrentTime,
+  setIsSeeking,
 }) => {
-  // Local states
   const seekBarRef = useRef<HTMLDivElement | null>(null);
   const progressBarRef = useRef<HTMLDivElement | null>(null);
 
   const [isDragging, setIsDragging] = useState<boolean>(false);
+  const [seekPosition, setSeekPosition] = useState<number | null>(null);
 
   useEffect(() => {
-    if (
-      !isDragging &&
-      currentHowl &&
-      seekBarRef.current &&
-      progressBarRef.current
-    ) {
+    if (!isDragging && currentHowl && seekBarRef.current && progressBarRef.current) {
       const seekBar = seekBarRef.current;
       const progressBar = progressBarRef.current;
       const durationInSeconds = fullDuration || 0;
-
       const progressPercentage = (currentTime / durationInSeconds) * 100;
       const initialTranslation = `translateX(${progressPercentage}%)`;
 
@@ -45,16 +41,30 @@ const PlaybackBar: React.FC<TimeProps> = ({
     }
   }, [currentTime, currentHowl, fullDuration, isDragging]);
 
+  useEffect(() => {
+    const handleGlobalMouseUp = () => {
+      if (isDragging && currentHowl && seekPosition !== null) {
+        currentHowl.seek(seekPosition);
+        setIsDragging(false);
+        setIsSeeking(false);
+        setSeekPosition(null);
+      }
+    };
+
+    document.addEventListener("mouseup", handleGlobalMouseUp);
+
+    return () => {
+      document.removeEventListener("mouseup", handleGlobalMouseUp);
+    };
+  }, [isDragging, seekPosition, currentHowl, setIsSeeking]);
+
   const handleSeekStart = () => {
-    console.log("Seek Start");
     setIsDragging(true);
+    setIsSeeking(true);
   };
 
-  const handleSeekRelease: React.MouseEventHandler<HTMLDivElement> = (event) => {
-    console.log("Seek Release");
-    setIsDragging(false);
-
-    if (currentHowl && seekBarRef.current) {
+  const handleSeekDrag: React.MouseEventHandler<HTMLDivElement> = (event) => {
+    if (isDragging && seekBarRef.current) {
       const seekBar = seekBarRef.current;
       const progressBar = progressBarRef.current;
       const boundingBox = seekBar.getBoundingClientRect();
@@ -63,9 +73,8 @@ const PlaybackBar: React.FC<TimeProps> = ({
       const durationInSeconds = fullDuration || 0;
       const newSeekPosition = newPosition * durationInSeconds;
 
-      if (!isDragging) {
-        currentHowl.seek(newSeekPosition);
-      }
+      setSeekPosition(newSeekPosition);
+      setCurrentTime(newSeekPosition);
 
       if (progressBar) {
         const progressPercentage = (newSeekPosition / durationInSeconds) * 100;
@@ -74,10 +83,9 @@ const PlaybackBar: React.FC<TimeProps> = ({
     }
   };
 
-  const handleSeekDrag: React.MouseEventHandler<HTMLDivElement> = (event) => {
-    if (isDragging && currentHowl && seekBarRef.current) {
+  const handleSeekClick: React.MouseEventHandler<HTMLDivElement> = (event) => {
+    if (!isDragging && currentHowl && seekBarRef.current) {
       const seekBar = seekBarRef.current;
-      const progressBar = progressBarRef.current;
       const boundingBox = seekBar.getBoundingClientRect();
       const newPosition =
         (event.clientX - boundingBox.left) / boundingBox.width;
@@ -85,10 +93,12 @@ const PlaybackBar: React.FC<TimeProps> = ({
       const newSeekPosition = newPosition * durationInSeconds;
 
       currentHowl.seek(newSeekPosition);
+      setSeekPosition(newSeekPosition);
+      setCurrentTime(newSeekPosition);
 
-      if (progressBar) {
+      if (progressBarRef.current) {
         const progressPercentage = (newSeekPosition / durationInSeconds) * 100;
-        progressBar.style.transform = `translateX(${progressPercentage}%)`;
+        progressBarRef.current.style.transform = `translateX(${progressPercentage}%)`;
       }
     }
   };
@@ -98,8 +108,8 @@ const PlaybackBar: React.FC<TimeProps> = ({
       className="seekBar"
       ref={seekBarRef}
       onMouseDown={handleSeekStart}
-      onMouseUp={handleSeekRelease}
       onMouseMove={handleSeekDrag}
+      onClick={handleSeekClick}
     >
       <div className="progressBar" ref={progressBarRef}></div>
     </div>
--- client/src/components/PlaylistPages/music-controller/time-controls/TimeControls.tsx
+++ client/src/components/PlaylistPages/music-controller/time-controls/TimeControls.tsx
@@ -3,56 +3,60 @@ import React, { useEffect, useState } from "react";
 import PlaybackBar from "./PlayBar";
 import "./TimeControls.scss";
 
-
 interface TimeProps {
   currentHowl: Howl | null;
   fullDuration: number | null;
 }
 
+// Helper function to format time in minutes and seconds
+function timeFormatter(timeInSeconds: number): string {
+  const minutes = Math.floor(timeInSeconds / 60);
+  const seconds = Math.floor(timeInSeconds % 60);
+  return `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
+}
+
 const TimeControls: React.FC<TimeProps> = ({ currentHowl, fullDuration }) => {
-  const { Howl } = require("howler");
-
-  //Local states
+  // Local states
   const [currentTime, setCurrentTime] = useState<number>(0);
+  const [isSeeking, setIsSeeking] = useState<boolean>(false); // Track seeking state
 
-  //Refresh this component every time a new song gets picked
+  // This useEffect runs once when the component is mounted and clears the interval when the component unmounts
   useEffect(() => {
-    timerLogic(currentHowl);
-  }, [currentHowl]);
-
-  const timerLogic = (currentHowl: typeof Howl | null) => {
     let updateInterval: NodeJS.Timeout | null = null;
 
-    // Update the song timer in ms intervals
-    if (currentHowl) {
-      updateInterval = setInterval(() => {
-        if (currentHowl.playing !== undefined && currentHowl.playing()) {
-          const newCurrentTime = currentHowl.seek();
-          if (newCurrentTime >= 0) {
-            setCurrentTime(newCurrentTime);
+    const timerLogic = () => {
+      if (currentHowl) {
+        updateInterval = setInterval(() => {
+          if (!isSeeking && currentHowl.playing !== undefined && currentHowl.playing()) {
+            const newCurrentTime = currentHowl.seek();
+            if (newCurrentTime >= 0) {
+              setCurrentTime(newCurrentTime);
+            }
           }
-        }
-      }, 500);
-    }
+        }, 500);
+      }
+    };
+
+    timerLogic();
+
     return () => {
-      // Clear the interval when the component unmounts or when currentHowl changes
+      // Clear the interval when the component unmounts
       if (updateInterval) {
         clearInterval(updateInterval);
       }
     };
-  };
-  // Format time in minutes and seconds
-  function timeFormatter(timeInSeconds: number) {
-    const minutes = Math.floor(timeInSeconds / 60);
-    const seconds = Math.floor(timeInSeconds % 60);
-    return `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
-  }
+  }, [currentHowl, isSeeking]);
 
   return (
-    // prettier-ignore
     <div className="songTimeElements">
       <div className="currTime">{timeFormatter(currentTime)}</div>
-      <PlaybackBar currentHowl={currentHowl} currentTime={currentTime} fullDuration={fullDuration}/>
+      <PlaybackBar
+        currentHowl={currentHowl}
+        currentTime={currentTime}
+        fullDuration={fullDuration}
+        setCurrentTime={setCurrentTime}
+        setIsSeeking={setIsSeeking} // Pass setIsSeeking to PlaybackBar
+      />
       <div className="endTime">
         {fullDuration !== null ? timeFormatter(fullDuration) : "0:00"}
       </div>
